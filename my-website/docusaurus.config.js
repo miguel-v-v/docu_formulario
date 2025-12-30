9c6177ec7b06a0928aaae5ac01080018a2d081bb
miguel-v-v <miguelvergara393@gmail.com>
2025-12-30 13:14:29 -0600

    Actualiza configuracion

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docu_formulario',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'miguel-v-v', // Usually your GitHub org/user name.
+  projectName: 'docu_formulario', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
